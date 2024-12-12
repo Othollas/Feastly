ed7712c23e235eb3b844bac705e23aed93ec367d
o_thollas <o_thollas@hotmail.com>
2024-12-12 16:09:18 +0100

    modif appel api coté client

--- server/server.js
+++ server/server.js
@@ -8,13 +8,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/api', apiRoutes);
-
 app.get('/', (req, res) => {
   res.send('Backend déployé avec succès avec server.js !');
 });
 
-const PORT = process.env.PORT || 3000;
+app.use('/api', apiRoutes);
+
+
+
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
